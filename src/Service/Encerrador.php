48fbd7d75cc7ead42f8edb178553ed719fc79546
Ismael Barboza <ismael@inovadora.com.br>
2021-01-18 18:05:50 -0300

    Add Stubs to EncerradorTest

---
+++ src/Service/Encerrador.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Leilao\Service;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+
+class Encerrador
+{
+    public function __construct(LeilaoDao $dao) {
+        $this->dao = $dao;
+    }
+    public function encerra()
+    {
+        $leiloes = $this->dao->recuperarNaoFinalizados();
+
+        foreach ($leiloes as $leilao) {
+            if ($leilao->temMaisDeUmaSemana()) {
+                $leilao->finaliza();
+                $this->dao->atualiza($leilao);
+            }
+        }
+    }
+}
---
+++ tests/Service/EncerradorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Alura\Leilao\Dao\Leilao as DaoLeilao;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Service\Encerrador;
+use PHPUnit\Framework\TestCase;
+
+class EncerradorTest extends TestCase {
+    public function testEncerrarLeilao() {
+        $leilao1 = new Leilao('Fiat 147', new DateTimeImmutable('8 days ago'));
+        $leilao2 = new Leilao('Variant', new DateTimeImmutable('10 days ago'));
+        
+        $stub = $this->createMock(DaoLeilao::class);
+        $stub->method('recuperarNaoFinalizados')->willReturn([$leilao1, $leilao2]);
+        $stub->method('recuperarFinalizados')->willReturn([$leilao1, $leilao2]);
+        
+        $encerrador = new Encerrador($stub);
+        
+        $encerrador->encerra();
+        
+        /** @var Leilao[] $leiloes */
+        $leiloes = [$leilao1, $leilao2];
+
+        self::assertCount(2, $leiloes);
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
+    }
+}
