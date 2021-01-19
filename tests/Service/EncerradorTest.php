9b9b845cf6252ad746cfd603b3325f5f79a68207
Ismael Barboza dos Santos <ismabarboza@gmail.com>
2021-01-19 07:34:55 -0300

    Modulo 2

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -3,6 +3,7 @@
 use Alura\Leilao\Dao\Leilao as DaoLeilao;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Service\Encerrador;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class EncerradorTest extends TestCase {
@@ -10,10 +11,16 @@ class EncerradorTest extends TestCase {
         $leilao1 = new Leilao('Fiat 147', new DateTimeImmutable('8 days ago'));
         $leilao2 = new Leilao('Variant', new DateTimeImmutable('10 days ago'));
         
+        /**@var MockObject */
         $stub = $this->createMock(DaoLeilao::class);
         $stub->method('recuperarNaoFinalizados')->willReturn([$leilao1, $leilao2]);
         $stub->method('recuperarFinalizados')->willReturn([$leilao1, $leilao2]);
         
+        $stub->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withConsecutive([$leilao1], [$leilao2]);
+
+
         $encerrador = new Encerrador($stub);
         
         $encerrador->encerra();
